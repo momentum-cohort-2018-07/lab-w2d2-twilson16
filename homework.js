6292a66c06043e37434ac2e5a95a7db6fd02f9af
Tina <christinawilson6@gmail.com>
2018-08-07 16:42:38 -0400

    answers 1-5

--- homework.js
+++ homework.js
@@ -2,20 +2,60 @@
 // returns the sum of those numbers.
 
 function sum (numbers) {
-  console.log('numbers', numbers)
-  return 0
-}
+  var totalSum = 0
+  for (var i = 0; i < numbers.length; i++) {
+    totalSum += numbers[i];
+  }
+    return totalSum
+  }
 
 // 2. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+
+
+function average (numbers) {
+  var sum = 0
+  if (numbers.length === 0) {return undefined}
+  else if (numbers.length === 1) {return numbers[0]}
+  for (var i = 0; i <= numbers.length-1; i++) {
+    sum += numbers[i];
+  } 
+return sum/numbers.length
+}
 
 // 3. Create a function called "intersection" that takes two arrays and
 // returns a new array that contains the elements found in both arrays.
 // The order they are in does not matter, but no duplicates should be
 // in the returned array, even if they were in the input.
 
+function intersection (array1, array2) {
+    var output = []
+    for (var i1 = 0; i1 < array1.length; i1++) {
+      for (var i2 = 0; i2 < array2.length; i2++) {
+        if (array1[i1] === array2[i2]) {
+          output.push(array1[i1])
+        }
+      } 
+    } 
+return output
+
+}
+
 // 4. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+function minimum (array) {
+  var min 
+  if (array.length === 0) {return undefined} 
+  else if (array.length === 1) {return array[0]}
+  array.sort()
+  for (var i = 0; i < array.length; i++) {
+    if ((typeof(min) === 'undefined') || (array[i] < min)) {
+      min = array[i];
+    }
+  }
+return min
+}
 
 // 5. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -40,6 +80,58 @@ function sum (numbers) {
 // Note 2: Selection sort can be implemented using one array. Read the explanation at
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
+
+// function selectionSort (array) {
+//   var output = []
+//   var arrayCopy = array.slice()
+//   if (array.length === 0) {return array[0]}
+//   if (array.length === 1) {return array[0]}
+//   array.sort()
+//   for (var i = 0; i < array.length; i++) {
+//   for (var i = 0; i < array.length; i++) {
+//       arrayCopy }
+//    output.push(array[i])
+//   }
+// }
+// return output
+// }
+
+
+// //copy original array
+// var arrayCopy = array.slice(0)
+// var sortedArray = []
+
+// //loop through new array (copy)
+// function copyMin (arrayCopy) {
+//  for (var i = 0; i < array.length; i++) {
+//    //find min number in array copy 
+//    call(minimum) {
+//    //insert into output array
+//    sortedArray.push(array) }
+//    //delete from array copy
+// }
+// delete arrayCopy
+// //return output array
+// return sortedArray
+// }
+
+function selectionSort (array) {
+var arrayCopy = array.slice(0)
+var sortedArray = []
+var idxOf;
+var min;
+var i = 0;
+if (array.length === 0) {return array}
+else if (array.length === 1) {return [2]}
+while (i <= array.length-1) { 
+   min = minimum(arrayCopy);
+   sortedArray.push(min);
+   idxOf = arrayCopy.indexOf(min);
+   arrayCopy.splice(idxOf,1)
+   i++
+ }
+return sortedArray
+}
 
 // 6. Create a function called "createUser" that takes a name and a Date object
 // and returns an object with the keys "name" and "dob" (date of birth) with
